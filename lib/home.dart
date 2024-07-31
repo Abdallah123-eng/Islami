44adf886ff6606f61bfe2811b13b5e609fec468f
THE LAPTOP SHOP <abdallahmohamedabuelenein@gmail.com>
2024-07-31 20:04:48 +0300

    app init

--- lib/home.dart
+++ lib/home.dart
@@ -1,17 +1,24 @@
 import 'package:flutter/material.dart';
 
 class HomeScreen extends StatelessWidget {
-  static const String routeName="Islami";
+  static const String routeName="home";
   const HomeScreen({super.key});
 
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-
-        title: Text("Islami sgerge"),
-      ),
+    return Stack(
+      children: [
+        Image.asset("assets/images/main_bg.png"),
+        Scaffold(
+          backgroundColor: Colors.transparent,
+          appBar: AppBar(
+            backgroundColor: Colors.transparent,
+            centerTitle: true,
+            title: Text("Islami",style: TextStyle(color: Colors.black),),
+          ),
+        ),
+      ],
     );
   }
 }
